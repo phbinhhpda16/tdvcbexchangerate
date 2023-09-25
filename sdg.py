ec8af2916a32f83848c83816d59ddefe2c8b492a
phbinhhpda16 <97727700+phbinhhpda16@users.noreply.github.com>
2023-09-25 17:30:11 +0700

    Update sdg.py

--- sdg.py
+++ sdg.py
@@ -1,7 +1,27 @@
 from tvDatafeed import TvDatafeed, Interval
+from shareplum import Site
+from shareplum import Office365
 
 tv = TvDatafeed('', '')
 
-nifty_index_data = tv.get_hist(symbol='AKS1!',exchange='NYMEX',interval=Interval.in_daily,n_bars=5000)
+data = tv.get_hist(symbol='AKS1!',exchange='NYMEX',interval=Interval.in_daily,n_bars=1)
 
-print(nifty_index_data["close"])
+usdjpn = tv.get_hist(symbol='USDJPY',exchange='OANDA',interval=Interval.in_daily,n_bars=1)
+
+
+def data_extract(symbol, exchange,sp_site,user_name,password,site_url,list):
+    data = tv.get_hist(symbol=symbol,exchange=exchange,interval=Interval.in_daily,n_bars=1)
+    data.reset_index(inplace= True)
+    data.rename(columns={'datetime': 'Title'}, inplace=True)
+    data = data[["Title", "close"]]
+    data_dict = data.to_dict(orient = 'records')
+    authcookie = Office365(sp_site, username=user_name, password=password).GetCookies()
+    site = Site(site_url, authcookie=authcookie)
+    sp_list = site.List(list)
+    sp_list.UpdateListItems(data=data_dict, kind='New')
+    print("Done!")
+
+data_extract('AKS1!', 'NYMEX','https://datapot01.sharepoint.com','binhph@datapot01.onmicrosoft.com','@1L0v3900','https://datapot01.sharepoint.com/sites/Test',"sdg")
+data_extract('AKS1!', 'NYMEX','https://datapot01.sharepoint.com','binhph@datapot01.onmicrosoft.com','@1L0v3900','https://datapot01.sharepoint.com/sites/Test',"usdjpn")
+
+
