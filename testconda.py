4be1bd4badf140b8589019463331348e7ecee28c
phbinhhpda16 <97727700+phbinhhpda16@users.noreply.github.com>
2023-10-09 10:39:12 +0700

    Create testconda.py

---
+++ testconda.py
@@ -0,0 +1,9 @@
+from tvDatafeed import TvDatafeed, Interval
+import pandas as pd
+import datetime as dt
+from bs4 import BeautifulSoup
+from shareplum import Site
+from shareplum import Office365
+import requests
+import urllib3
+import ssl
